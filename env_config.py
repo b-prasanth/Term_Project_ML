40266bbe900cacb61d994cbfc27b62d5eea1cadb
b-prasanth <97497164+b-prasanth@users.noreply.github.com>
2024-12-04 20:52:45 -0500

    Delete env_config.py

--- env_config.py
+++
@@ -1,9 +0,0 @@
-import warnings
-import pandas as pd
-
-#Setting up pandas display settings
-pd.options.mode.copy_on_write = True
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.options.display.float_format = '{:.3f}'.format
-warnings.filterwarnings('ignore')
